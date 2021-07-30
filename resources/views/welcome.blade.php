84ea3a1e23bb5010fde8ac79f95c47f0e381fabc
Sparclex <silvan@kbuhl.ch>
2021-07-30 09:17:54 +0200

    Add feature section to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,10 +27,7 @@
 
         <div class="relative pt-6 pb-16 sm:pb-24">
             <x-guest-navigation
-            :links="[
-                'Features' => '#',
-                'About' => '#',
-            ]"
+                    :links="[]"
             />
 
             <div class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
@@ -52,4 +49,123 @@
             </div>
         </div>
     </div>
+
+    <div class="relative bg-white pt-16 pb-32 overflow-hidden">
+        <div class="relative">
+            <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+                    <div>
+                        <div>
+                            <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                                <!-- Heroicon name: outline/inbox -->
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"/>
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="mt-6">
+                            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
+                                Validate Rdml files
+                            </h2>
+                            <p class="mt-4 text-lg text-gray-500">
+                                Validate your experiment results using the control values and quickly identify invalid
+                                experiments. Just by uploading a rdml file you may discover which targets and how many
+                                samples it contains. By relying on a standard like <a href="https://rdml.org"
+                                                                                      target="_blank"
+                                                                                      class="text-indigo-600 underline">Rdml</a>
+                                it works for data provided from different kinds of qPCR machines.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-12 sm:mt-16 lg:mt-0">
+                    <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                        <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                             src="{{ asset('/img/experiments.png') }}" alt="Experiment overview">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mt-24">
+            <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+                    <div>
+                        <div>
+                            <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                                <!-- Heroicon name: outline/sparkles -->
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"/>
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="mt-6">
+                            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
+                                Evaluate your data
+                            </h2>
+                            <p class="mt-4 text-lg text-gray-500">
+                                Quickly see all the results for an assay and identify errors. You can exclude outliners
+                                or download the results as an excel file for further evaluation.
+                                An api provides full flexability to do with the data whatever you like in the
+                                programming language of your choice.
+                            </p>
+                            <div class="mt-6">
+                                <a href="#"
+                                   class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+                                    Get started
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+                    <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                        <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                             src="{{asset('/img/results.png')}}"
+                             alt="Results overview">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="bg-gray-50">
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <span class="block">Ready to dive in?</span>
+                <span class="block text-indigo-600">Register today and wait for approval.</span>
+            </h2>
+            <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+                <div class="inline-flex rounded-md shadow">
+                    <a href="{{route('register')}}" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                        Get started
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <footer class="bg-white">
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+            <div class="flex justify-center space-x-6 md:order-2">
+                <a href="https://github.com/ELIMU-MDx/elimu-v2" class="text-gray-400 hover:text-gray-500">
+                    <span class="sr-only">GitHub</span>
+                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                    </svg>
+                </a>
+            </div>
+            <div class="mt-8 md:mt-0 md:order-1">
+                <p class="text-center text-base text-gray-400">
+                    &copy; 2021 Tobias Schindler &amp; Silvan Krähenbühl. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+
 </x-guest-layout>

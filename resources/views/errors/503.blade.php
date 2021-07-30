265df9d728ef90ced5e220cbde81a98c22cf73ee
Sparclex <silvan@kbuhl.ch>
2021-07-30 09:17:36 +0200

    Add maintenance site

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,15 @@
+<x-guest-layout>
+    <div class="bg-white min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div class="max-w-max mx-auto">
+            <main class="sm:flex">
+                <p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">503</p>
+                <div class="sm:ml-6">
+                    <div class="sm:border-l sm:border-gray-200 sm:pl-6">
+                        <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Down for maintenance</h1>
+                        <p class="mt-1 text-base text-gray-500">We are currently down for maintenance. Please check again in a few minutes</p>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</x-guest-layout>

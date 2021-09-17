b5484fe96cea31564ace0ea4f5024af75e61acc6
Sparclex <silvan@kbuhl.ch>
2021-09-17 13:36:43 +0200

    Allow unauthenticated support messages

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/assays/{assay}/results', ListResultController::class)->name('api.results.index');
-    Route::supportBubble();
 });
+Route::supportBubble();

5926a6e6354ce5aa236665d72ba70a58e1e59114
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-03 13:54:55 +0200

    Add test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use Domain\Study\Models\Study;
+use Domain\Study\Roles\Owner;
+use Domain\Users\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::query()->delete();
+        Study::query()->delete();
+        $user = User::create([
+            'name' => 'Silvan',
+            'email' => 'silvan@kbuhl.ch',
+            'email_verified_at' => now(),
+            'password' => Hash::make('bFAojbkDNokvPGRZP7e8'),
+        ]);
+
+        $study = Study::firstOrCreate([
+            'name' => 'Test Study',
+            'identifier' => 'testStudy'
+        ]);
+        $study->users()->attach($user->id, ['role' => new Owner()]);
         // \App\Models\User::factory(10)->create();
     }
 }

6b130bc8c2c5b00c3c1f4a2b83dcd5adb2c08ad6
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-17 14:17:03 +0200

    🐛 Show samples in colors

--- src/App/View/Livewire/ShowSample.php
+++ src/App/View/Livewire/ShowSample.php
@@ -25,32 +25,39 @@ class ShowSample extends Component
         $this->sample = $sample;
 
         $this->sample->load(['measurements.dataPoints', 'measurements.experiment']);
-        $this->filters['position'] = $this->sample->measurements->pluck('position')->unique()->toArray();
-        $this->filters['target'] = $this->sample->measurements->pluck('target')->unique()->toArray();
-        $this->filters['experiment'] = $this->sample->measurements->pluck('experiment.name')->unique()->toArray();
+        $this->filters['position'] = $this->sample->measurements->pluck('position')->unique()->values()->toArray();
+        $this->filters['target'] = $this->sample->measurements->pluck('target')->unique()->values()->toArray();
+        $this->filters['experiment'] = $this->sample->measurements->pluck('experiment.name')->unique()->values()->toArray();
+    }
+
+    public function dehydrateFilters($value)
+    {
+        ray($value);
     }
 
     private function getFilteredMeasurements()
     {
+        ray($this->filters);
         $factory = app(MeasurementFilterFactory::class);
         $filters = collect($this->filters)
-            ->map(fn ($arguments, $filterName) => $factory->get($filterName, [$arguments]));
+            ->map(fn($arguments, $filterName) => $factory->get($filterName, [$arguments]));
 
         return $this->sample
             ->measurements
-            ->reject(fn (Measurement $measurement) => $filters
-                    ->first(fn (MeasurementFilter $filter) => ! $filter->matches($measurement)) !== null
+            ->reject(fn(Measurement $measurement) => $filters
+                    ->first(fn(MeasurementFilter $filter) => !$filter->matches($measurement)) !== null
             );
     }
 
     public function getSeriesProperty(): array
     {
         return $this->getFilteredMeasurements()
-            ->map(fn (Measurement $measurement) => [
+            ->map(fn(Measurement $measurement) => [
                 'name' => "$measurement->position $measurement->target",
+                'color' => $this->graphColor($measurement->position, $measurement->target),
                 'data' => $measurement
                     ->dataPoints
-                    ->map(fn (DataPoint $dataPoint) => $dataPoint->fluor)
+                    ->map(fn(DataPoint $dataPoint) => $dataPoint->fluor)
                     ->toArray(),
             ])
             ->sortBy('name')
@@ -58,9 +65,27 @@ class ShowSample extends Component
             ->toArray();
     }
 
+    private function graphColor(string $position, string $target): string
+    {
+        $colors = [
+            // lime
+            0 => ['#a3e635', '#65a30d', '#365314'],
+            // blue
+            1 => ['#60a5fa', '#2563eb', '#1e3a8a'],
+            // red
+            2 => ['#fb7185', '#e11d48', '#881337'],
+            // purple
+            3 => ['#c084fc', '#9333ea', '#581c87'],
+        ];
+
+        $category = $colors[$this->sample->measurements->pluck('position')->unique()->sort()->values()->search($position) % 4];
+        return $category[$this->sample->measurements->pluck('target')->sort()->unique()->values()->search($target) % 3];
+    }
+
     public function render()
     {
         $options = [
+            'colors' => ['#F44336', '#F44336', '#F44336', '#FF00FF', '#FF00FF', '#FF00FF'],
             'chart' => [
                 'type' => 'line',
                 'zoom' => [

f3180c69a7bb935ec2c9fc22571f79903c71a68f
Sparclex <Sparclex@users.noreply.github.com>
2022-10-18 08:53:56 +0000

    🎨 Apply laravel pint styles [ci skip]

--- src/App/View/Livewire/ShowSample.php
+++ src/App/View/Livewire/ShowSample.php
@@ -34,24 +34,24 @@ class ShowSample extends Component
     {
         $factory = app(MeasurementFilterFactory::class);
         $filters = collect($this->filters)
-            ->map(fn($arguments, $filterName) => $factory->get($filterName, [$arguments]));
+            ->map(fn ($arguments, $filterName) => $factory->get($filterName, [$arguments]));
 
         return $this->sample
             ->measurements
-            ->reject(fn(Measurement $measurement) => $filters
-                    ->first(fn(MeasurementFilter $filter) => !$filter->matches($measurement)) !== null
+            ->reject(fn (Measurement $measurement) => $filters
+                    ->first(fn (MeasurementFilter $filter) => ! $filter->matches($measurement)) !== null
             );
     }
 
     public function getSeriesProperty(): array
     {
         return $this->getFilteredMeasurements()
-            ->map(fn(Measurement $measurement) => [
+            ->map(fn (Measurement $measurement) => [
                 'name' => "$measurement->target $measurement->position",
                 'color' => $this->graphColor($measurement->position, $measurement->target),
                 'data' => $measurement
                     ->dataPoints
-                    ->map(fn(DataPoint $dataPoint) => $dataPoint->fluor)
+                    ->map(fn (DataPoint $dataPoint) => $dataPoint->fluor)
                     ->toArray(),
             ])
             ->sortBy('name')

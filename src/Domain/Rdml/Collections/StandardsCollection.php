1557ed91e5f6e4ae9eed6d14f1807f54f0d005cc
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-21 15:36:19 +0200

    Ignore standards values with null cq value

--- src/Domain/Rdml/Collections/StandardsCollection.php
+++ src/Domain/Rdml/Collections/StandardsCollection.php
@@ -9,26 +9,29 @@ use Illuminate\Support\Collection;
 
 final class StandardsCollection extends Collection
 {
-    public function dataPoints(): Collection
+    public function nonNullDataPoints(): Collection
     {
-        return $this->map(fn (Measurement $measurement) => [
-            'x' => log10($measurement->quantity), 'y' => $measurement->cq,
-        ])
+        return $this
+            ->filter(fn(Measurement $measurement) => $measurement->cq)
+            ->map(fn(Measurement $measurement) => [
+                'x' => log10($measurement->quantity), 'y' => $measurement->cq,
+            ])
+            ->values()
             ->toBase();
     }
 
     public function linearRegression(): array
     {
-        return linear_regression($this->dataPoints()->toArray());
+        return linear_regression($this->nonNullDataPoints()->toArray());
     }
 
     public function correlationCoefficient(): float|int
     {
-        $dataPoints = $this->dataPoints();
+        $dataPoints = $this->nonNullDataPoints();
 
         return (new Correlation())
-        ->pearson($dataPoints->pluck('x')->toArray(), $dataPoints->pluck('y')->toArray())
-        ->coefficient;
+            ->pearson($dataPoints->pluck('x')->toArray(), $dataPoints->pluck('y')->toArray())
+            ->coefficient;
     }
 
     public function quantifyConfiguration(): QuantifyConfiguration

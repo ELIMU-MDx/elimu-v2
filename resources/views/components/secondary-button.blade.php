c60db5e68ca74c149a8284321e416656f6b4ad57
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-25 15:40:25 +0200

    Decrease secondary button font size

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,5 +1,5 @@
 <{{$as}} {{$attributes->merge([
-    'class' => 'w-full flex items-center justify-center px-4 py-2 border border-indigo-600 text-base font-medium rounded-md text-indigo-600 cursor-pointer hover:bg-indigo-100',
+    'class' => 'w-full flex items-center justify-center px-4 py-2 text-sm border border-indigo-600 font-medium rounded-md text-indigo-600 cursor-pointer hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
     'tabindex' => '0'
     ])}}>
 {{$slot}}

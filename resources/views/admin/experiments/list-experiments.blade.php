8eff9e89a471593ecaf5765843da9b2dc8ee8970
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-25 15:37:01 +0200

    Increase secondary button size

--- resources/views/admin/experiments/list-experiments.blade.php
+++ resources/views/admin/experiments/list-experiments.blade.php
@@ -16,23 +16,28 @@
             <div>
                 <div class="relative inline-block text-left" x-data="{open: false, sort: @entangle('sort')}" wire:>
                     <div>
-                        <button type="button"
-                                @click="open = !open"
-                                class="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900"
-                                id="menu-button" :aria-expanded="open" aria-haspopup="true">
+                        <x-secondary-button
+                            as="button"
+                            type="button"
+                            @click="open = !open"
+                            id="menu-button"
+                            x-bind:aria-expanded="open"
+                            aria-haspopup="true"
+                        >
                             Sort
                             <!-- Heroicon name: mini/chevron-down -->
-                            <svg class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500 transition-all duration-200"
-                                 :class="{
+                            <svg
+                                class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-indigo-600 group-hover:text-gray-500 transition-all duration-200"
+                                :class="{
                                  'rotate-180': open
                                  }"
-                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                 aria-hidden="true">
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
                                 <path fill-rule="evenodd"
                                       d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
                                       clip-rule="evenodd"/>
                             </svg>
-                        </button>
+                        </x-secondary-button>
                     </div>
 
                     <div
@@ -62,8 +67,9 @@
                                 :class="sort === 'name' ? 'font-medium text-gray-900' : 'text-gray-500'"
                                 @click="open=false"
                                 wire:click.prevent="updateSort('name')"
-                                href="#" class="hover:bg-gray-100 text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-                               id="menu-item-1">Name</a>
+                                href="#" class="hover:bg-gray-100 text-gray-500 block px-4 py-2 text-sm" role="menuitem"
+                                tabindex="-1"
+                                id="menu-item-1">Name</a>
                         </div>
                     </div>
                 </div>
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,5 +1,5 @@
 <{{$as}} {{$attributes->merge([
-    'class' => 'w-full flex items-center justify-center px-8 py-3 border border-indigo-600 text-base font-medium rounded-md text-indigo-600 cursor-pointer hover:bg-indigo-100 md:py-4 md:text-lg md:px-10',
+    'class' => 'w-full flex items-center justify-center px-4 py-2 border border-indigo-600 text-base font-medium rounded-md text-indigo-600 cursor-pointer hover:bg-indigo-100',
     'tabindex' => '0'
     ])}}>
 {{$slot}}

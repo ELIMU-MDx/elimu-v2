9fc9f546157302243f3c8846b46077125d679403
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-27 13:39:37 +0200

    Add recalculate result command

---
+++ src/App/Console/Commands/RecalculateResultCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Domain\Experiment\Actions\RecalculateResultsAction;
+use Domain\Experiment\Models\Experiment;
+use Domain\Experiment\Models\Measurement;
+use Domain\Experiment\Models\Sample;
+use Illuminate\Console\Command;
+
+class RecalculateResultCommand extends Command
+{
+    protected $signature = 'elimu:recalculate {studyId}';
+
+    protected $description = 'Recalculates the results for a study';
+
+    public function handle(RecalculateResultsAction $action): int
+    {
+        $measurements = Measurement::whereHas('sample', fn($query) => $query->where('study_id', $this->argument('studyId')))
+            ->get();
+
+        $this->info("Recaluclate results for {$measurements->count()} measurements");
+
+        $action->execute($measurements);
+
+        $this->info("Successfully recalculated results");
+
+        return 0;
+    }
+}

5d46fb0426a3f789646d01a7be854fef00d8bdf6
Sparclex <Sparclex@users.noreply.github.com>
2022-10-28 08:10:21 +0000

    🎨 Apply laravel pint styles [ci skip]

--- tests/Feature/Result/ShowSampleReportTest.php
+++ tests/Feature/Result/ShowSampleReportTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Database\Factories\SampleFactory;
-use Illuminate\Routing\Middleware\ValidateSignature;
 
 it('shows a pdf report for a sample', function () {
     $sample = SampleFactory::new()

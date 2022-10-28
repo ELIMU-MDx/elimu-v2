da0d12f667d387b4e76d7e76c5885ba1adf95182
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-10-28 08:53:49 +0200

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,9 @@
                                 than ever</span>
                         </h1>
                         <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                            ELIMU-MDx (Electronic Laboratory Information & Management Utensil for Molecular Diagnostics)
+                            <strong class="font-bold">ELIMU-MDx</strong> (<strong class="font-bold">E</strong>lectronic
+                            <strong class="font-bold">L</strong>aboratory <strong class="font-bold">I</strong>nformation & <strong class="font-bold">M</strong>anagement
+                            <strong class="font-bold">U</strong>tensil for <strong class="font-bold">M</strong>olecular <strong class="font-bold">D</strong>iagnostics)
                             is an open-source and user-friendly platform designed and built to accelerate the turnaround
                             time of diagnostic qPCR assay analysis.
                         </p>

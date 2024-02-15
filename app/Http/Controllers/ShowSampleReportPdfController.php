3eeecd3e2b2dff6f94a9a4fab60b2620036a34c5
Silvan Krähenbühl <silvan@kbuhl.ch>
2024-02-15 17:11:51 +0100

    wip

--- app/Http/Controllers/ShowSampleReportPdfController.php
+++ app/Http/Controllers/ShowSampleReportPdfController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Assay;
 use App\Models\Sample;
 use Domain\Study\Actions\CreateSampleReportAction;
+use Illuminate\Support\Facades\URL;
 use Spatie\Browsershot\Browsershot;
 
 final class ShowSampleReportPdfController
@@ -15,10 +16,10 @@ final class ShowSampleReportPdfController
 
     public function __invoke(Assay $assay, Sample $sample)
     {
-        $report = $this->createReport->execute($assay, $sample);
-
         return response(
-            Browsershot::html(view('samples.report', compact('report'))->render())
+            Browsershot::url(
+                URL::temporarySignedRoute('samples.report', now()->addMinute(), compact('sample', 'assay'))
+            )
                 ->pages('1-2')
                 ->emulateMedia('print')
                 ->pdf()

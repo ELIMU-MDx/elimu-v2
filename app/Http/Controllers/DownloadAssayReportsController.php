b596fc46da03cc5f26901191ec65e511a6c460e3
Silvan Krähenbühl <silvan@kbuhl.ch>
2024-02-16 06:58:24 +0100

    wip

--- app/Http/Controllers/DownloadAssayReportsController.php
+++ app/Http/Controllers/DownloadAssayReportsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Assay;
+use Carbon\CarbonInterval;
 use Domain\Assay\Jobs\CreateSampleReportsJob;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,6 +13,10 @@ final class DownloadAssayReportsController
     {
         dispatch_sync(new CreateSampleReportsJob($assay, Auth::user()->email));
 
-        return 'You will receive a mail with the reports within the next few minutes.';
+        $numberOfResults = $assay->results()->count();
+
+        $durationEstimate = CarbonInterval::seconds(5 * $numberOfResults);
+
+        return 'You will receive a mail with the reports within about: '.$durationEstimate->forHumans();
     }
 }

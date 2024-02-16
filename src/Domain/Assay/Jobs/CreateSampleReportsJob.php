0fece0bbd0ec3bc38ec9699089cf5473839908b6
Silvan Krähenbühl <silvan@kbuhl.ch>
2024-02-16 06:51:03 +0100

    wip

--- src/Domain/Assay/Jobs/CreateSampleReportsJob.php
+++ src/Domain/Assay/Jobs/CreateSampleReportsJob.php
@@ -71,8 +71,6 @@ final class CreateSampleReportsJob implements ShouldBeUnique, ShouldQueue
                                 $index + 1,
                                 $totalChunks,
                             ));
-
-                            Storage::disk('local')->deleteDirectory('tmp/'.$batch->id);
                         });
                 })->all(),
                 function () use ($lock) {
